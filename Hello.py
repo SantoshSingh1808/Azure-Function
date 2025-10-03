cce94e4e15ac99f825f27cde4cd40ab00336a049
santosh <santosh1808.singh@gmail.com>
2025-10-03 09:42:45 +0530

    code

---
+++ Hello.py
@@ -0,0 +1,21 @@
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('यह Azure Function चालू हो गया है।')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            req_body = {}
+        name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"नमस्ते, {name}!", status_code=200)
+    else:
+        return func.HttpResponse(
+            "कृपया 'name' भेजें ताकि मैं नमस्ते कह सकूं।",
+            status_code=400
+        )
