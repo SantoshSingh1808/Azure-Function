91ae480ab6fdf11a455dbd9204d59849cb0a0519
santosh <santosh1808.singh@gmail.com>
2025-10-03 10:29:21 +0530

    Code updated

--- HelloWorld/__init__.py
+++ HelloWorld/__init__.py
@@ -2,15 +2,15 @@ import logging
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('यह Azure Function चालू हो गया है।')
+    logging.info("यह Azure Function चालू हो गया है।")
 
-    name = req.params.get('name')
+    name = req.params.get("name")
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             req_body = {}
-        name = req_body.get('name')
+        name = req_body.get("name")
 
     if name:
         return func.HttpResponse(f"नमस्ते, {name}!", status_code=200)
